65caecdd24489f1c3afb663739f38639381cfff2
hubert <hubert@Ubuntu.myguest.virtualbox.org>
2024-05-17 19:11:53 -0400

    Create function stubs for unit test file

---
+++ test/test_mem.c
@@ -0,0 +1,13 @@
+void test_req_mem() {
+	printf("Testing req_mem():\n");
+}
+
+void test_rel_mem() {
+	printf("Testing rel_mem():\n");
+}
+
+int main() {
+	test_req_mem();
+	test_rel_mem();
+	return 0;
+}
